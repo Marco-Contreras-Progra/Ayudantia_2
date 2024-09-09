51f56f3212f17bccf947412d298f6206b42a3b27
Marco Contreras <m.contreras28@ufromail.cl>
2024-09-09 18:21:06 -0300

    Commit inicial

---
+++ src/main/java/ayudantia2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ayudantia2 {
+    public static void main(String[] args) {
+
+        Object[][] productos = new Object[10][3];
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    public static void agregarProductos (int idProducto, int cantidad){
+
+    }
+
+    public static void restarProductos (int idProducto, int cantidad){
+
+    }
+
+    public static int consultarDisponibilidad (int idProducto){
+        return 0;
+    }
+
+    public static void listarProductos (){
+
+    }
+
+}

8a06206b4fa76002cda3ec5a4904dfbb9ed4f2dd
Marco Contreras <m.contreras28@ufromail.cl>
2024-09-10 00:17:03 -0300

    Commit con funciones hechas

--- src/main/java/ayudantia2.java
+++ src/main/java/ayudantia2.java
@@ -4,24 +4,71 @@ public class ayudantia2 {
     public static void main(String[] args) {
 
         Object[][] productos = new Object[10][3];
-        Scanner sc = new Scanner(System.in);
 
     }
+    public static void agregarProducto(Object[][] productos, String idProducto, int cantidad){
+        boolean productoEncontrado = false;
 
-    public static void agregarProductos (int idProducto, int cantidad){
+        for (int i = 0; i < productos.length; i++) {
+            if (productos[i][0] != null && productos[i][0].equals(idProducto)) {
+                int cantidadActual = (Integer) productos[i][2];
+                productos[i][2] = cantidadActual + cantidad;
+                productoEncontrado = true;
+                System.out.println("Cantidad actualizada para el producto con id: " + idProducto);
+                break;
+            }
+        }
+        if (!productoEncontrado) {
+            System.out.println("Producto con el id:" +idProducto +"no encontrado.");
+        }
+    }
+    public static void restarProductos(Object[][] productos, String idProducto, int cantidad) {
+        boolean productoEncontrado = false;
 
+        for (int i = 0; i < productos.length; i++) {
+            if (productos[i][0] != null && productos[i][0].equals(idProducto)) {
+                int cantidadActual = (Integer) productos[i][2];
+                try {
+                    if (cantidad > cantidadActual) {
+                        throw new Exception("Error: La cantidad a restar es mayor que la cantidad disponible");
+                    }
+                    productos[i][2] = cantidadActual - cantidad;
+                    productoEncontrado = true;
+                    System.out.println("Cantidad actualizada para el producto con id: " + idProducto);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            }
+        }
+        if (!productoEncontrado) {
+            System.out.println("Producto con el id:" +idProducto +"no encontrado.");
+        }
+    }
+    public static int consultarDisponibilidad(Object[][] productos, String idProducto) {
+        for (int i = 0; i < productos.length; i++) {
+            if (productos[i][0] != null && productos[i][0].equals(idProducto)){
+                return (Integer) productos[i][2];
+            }
+        }
+        System.out.println("Producto con el id:" +idProducto +"no encontrado.");
+        return -1;
+    }
+    public static void listarProductos(Object[][] productos) {
+        System.out.println("Listado de Productos:");
+        for (int i = 0; i < productos.length; i++) {
+            if (productos[i][0] != null) {
+                String idProducto = (String) productos[i][0];
+                String nombreProducto = (String) productos[i][1];
+                int cantidadDisponible = (Integer) productos[i][2];
+
+                System.out.println("id:"+ idProducto + "Nombre:" + nombreProducto + "Cantidad:" + cantidadDisponible);
+            }
+        }
     }
 
-    public static void restarProductos (int idProducto, int cantidad){
 
-    }
 
-    public static int consultarDisponibilidad (int idProducto){
-        return 0;
-    }
-
-    public static void listarProductos (){
-
-    }
+}
 
 }
